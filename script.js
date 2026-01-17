6b2546cab17bc76e67d39e7b6c798a69c99b1dcd
nilesh3294 <nileshgaikwad1411@gmail.com>
2026-01-17 23:55:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 function calculateBMI() {
-    var height = document.getElementById("height").value;
-    var weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+    let weight = document.getElementById("weight").value;
 
-    if (height == "" || weight == "") {
-        document.getElementById("result").innerHTML = "Please enter values";
+    if (height === "" || weight === "") {
+        document.getElementById("result").innerHTML = "Please enter all values";
         return;
     }
 
     height = height / 100;
-    var bmi = weight / (height * height);
-    bmi = bmi.toFixed(2);
+    let bmi = (weight / (height * height)).toFixed(2);
 
-    var msg = "";
+    let category = "";
+    let tips = "";
 
     if (bmi < 18.5) {
-        msg = "Underweight";
+        category = "Underweight";
+        tips = "You should eat more nutritious food and gain weight gradually.";
     } else if (bmi < 25) {
-        msg = "Normal";
+        category = "Normal";
+        tips = "Great! Maintain your healthy lifestyle.";
     } else if (bmi < 30) {
-        msg = "Overweight";
+        category = "Overweight";
+        tips = "You should exercise regularly, reduce sugary & oily food, and maintain a balanced diet.";
     } else {
-        msg = "Obese";
+        category = "Obese";
+        tips = "Consult a doctor or dietitian. Focus on controlled diet and regular exercise.";
     }
 
     document.getElementById("result").innerHTML =
-        "BMI = " + bmi + "<br>Status: " + msg;
+        "Your BMI is " + bmi + "<br>Category: " + category +
+        "<br><b>Tips:</b> " + tips;
 }
+
