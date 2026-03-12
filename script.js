a04c0fb5833930d7572d01fbb5c93fb9c614be3f
nilesh3294 <nileshgaikwad1411@gmail.com>
2026-03-12 10:09:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,4 +31,27 @@ function calculateBMI() {
         "Your BMI is " + bmi + "<br>Category: " + category +
         "<br><b>Tips:</b> " + tips;
 }
+function calculateCalories(){
+
+let weight = document.getElementById("weight").value;
+let height = document.getElementById("height").value;
+let age = document.getElementById("age").value;
+let gender = document.getElementById("gender").value;
+let activity = document.getElementById("activity").value;
+
+let bmr;
+
+if(gender === "male"){
+bmr = 10*weight + 6.25*height - 5*age + 5;
+}
+else{
+bmr = 10*weight + 6.25*height - 5*age - 161;
+}
+
+let calories = bmr * activity;
+
+document.getElementById("caloriesResult").innerHTML =
+"Your daily calorie need: " + Math.round(calories) + " kcal";
+
+}
 
